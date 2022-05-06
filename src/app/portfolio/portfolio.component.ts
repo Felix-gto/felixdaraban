9c7836b58212ce2a3dd715e3098bd3aabfbc2508
Felix <felix.daraban@gmail.com>
2022-05-06 13:38:26 +0300

    My Learning - Progress Bars completed - runs on mouseover

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,6 +8,9 @@ import { DarkModeService } from '../dark-mode.service';
 // Language Service
 import { LanguageService } from '../language.service';
 
+// HostListener for listening to scroll event to load progress bar
+import { HostListener } from '@angular/core';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -46,4 +49,66 @@ export class PortfolioComponent implements OnInit {
 
   }
 
+  /***** PROGRESS BARS *****/ 
+
+  // Counters
+  counter1 = 0;
+  counter2 = 0;
+  counter3 = 0;
+  counter4 = 0; 
+  
+  
+  onScrollIntoView() {
+    setInterval(() => {
+
+      // First counter
+      if(this.counter1 == 70){
+          clearInterval();
+      }else{
+          this.counter1 +=1;
+      }
+
+    }, 15);
+
+    setInterval(() => {
+
+      // Second counter
+      if(this.counter2 == 50){
+        clearInterval();
+      }else{
+          this.counter2 +=1;
+      }
+
+    }, 19);
+
+    setInterval(() => {
+
+      // Third counter
+      if(this.counter3 == 100){
+        clearInterval();
+      }else{
+          this.counter3 +=1;
+      }
+
+    }, 10);
+
+    setInterval(() => {
+
+      // Fourth counter
+      if(this.counter4 == 30){
+        clearInterval();
+      }else{
+          this.counter4 +=1;
+      }
+
+    }, 32.5);
+
+  }
+
+  onReset() {
+    this.counter1 = 0;
+    this.counter2 = 0;
+    this.counter3 = 0;
+    this.counter4 = 0;
+  }  
 }
