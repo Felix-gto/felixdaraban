73f4e9dec36b12189e8b049f1ca24887188c3ccc
Felix <felix.daraban@gmail.com>
2022-05-13 18:03:19 +0300

    CV - Initial setup completed

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+// Dark Mode Service
+import { DarkModeService } from '../dark-mode.service';
+
+// Language Service
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-cv',
@@ -7,9 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private darkModeService: DarkModeService,
+    private languageService: LanguageService
+  ) {}
 
-  ngOnInit(): void {
+  // darkModeSet property = darkModeOn property from the DarkModeService (true or false)
+  darkModeSet = this.darkModeService.darkModeOn;
+
+  // romanianSet property = romanianOn property from the Language Service (true or false)
+  romanianSet = this.languageService.romanianOn;
+
+  // Store the DarkModeService subscription
+  private darkModeSubscription: Subscription;
+
+  // Store the Language Service subscription
+  private languageSubscription: Subscription;
+
+  ngOnInit() {
+
+    // Subscribe to the DarkModeService Subject (darkModeStatusEmitter - observable) and listen to changes in the darkModeOn status... (changes on click emitted in the Navbar)
+    this.darkModeSubscription = this.darkModeService.darkModeStatusEmitter.subscribe(darkModeStatus => {
+      this.darkModeSet = darkModeStatus;
+    })
+
+    // Subscribe to the LanguageService Subject (languageEmitter - observable) and listen to changes in the language (Romanian or English)... (changes on click emitted in the Navbar)
+    this.languageSubscription = this.languageService.languageEmitter.subscribe(romanianOnStatus => {
+      this.romanianSet = romanianOnStatus;
+    })
+
   }
 
 }
